1520fed6a1dabed017dbc5eccbe717cab12dc4aa
Aaditya <aady.prakash@gmail.com>
2022-07-22 02:27:06 +0200

    Secret API Key

--- domain/src/main/java/com/aadi/pixabay/domain/utils/Config.kt
+++ domain/src/main/java/com/aadi/pixabay/domain/utils/Config.kt
@@ -1,8 +1,10 @@
 package com.aadi.pixabay.domain.utils
 
+import com.aadi.pixabay.domain.BuildConfig
+
 object Config {
     const val BASE_URL = "https://pixabay.com/"
-    const val API_KEY = "28644398-ad9c27bf3ca9a24bf3f948b8b"
+    const val API_KEY = BuildConfig.API_KEY
 
     const val TRIGGER_KEYWORD = "fruits"
     const val ITEMS_PER_PAGE = 30
